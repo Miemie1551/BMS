d6fa1b1f538d5d2063dc265875168318e7d80e05
lw <1504441551@qq.com>
2026-05-13 17:11:29 +0800

    feat(all): 数据采集显示成功

--- User/APP/DataAcqTask.c
+++ User/APP/DataAcqTask.c
@@ -1,12 +1,18 @@
 #include "DataAcqTask.h"
 #include "cmsis_os2.h"
 
-#include "usart.h"
-#include "bsp_bq76940.h"
 #include "bms_global.h"
 
-#define LOG_I(fmt, ...) printf("[INFO] " fmt "\r\n", ##__VA_ARGS__)
-#define LOG_E(fmt, ...) printf("[ERROR] " fmt "\r\n", ##__VA_ARGS__)
+#include "usart.h"
+#include "bsp_bq76940.h"
+
+#define LOG_I(fmt, ...) printf("[INFO] " fmt, ##__VA_ARGS__)
+#define LOG_D(fmt, ...) printf("[DEBUG] " fmt, ##__VA_ARGS__)
+#define LOG_E(fmt, ...) printf("[ERROR] [%s:%d]" fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+
+static void SOC_Calculation(void);
+static void Cell_Voltage_Analysis(void);
+static void PrintData(void);
 
 void DataAcqTask(void *argument)
 {
@@ -14,35 +20,86 @@ void DataAcqTask(void *argument)
     for (;;)
     {
         osDelay(1000);
-        if (BQ76940_ReadCellVoltages(g_bms_data.cell_voltages_array, &g_bms_data.battery_voltage) == 1)
+        if (BQ76940_ReadCellVoltages(g_bms_data.cell_voltages_array, &g_bms_data.battery_voltage) != 1)
         {
-            LOG_I("Cell Voltages: %d, %d, %d, %d, %d, %d, %d, %d, %d mV",
-                  g_bms_data.cell_voltages_array[0], g_bms_data.cell_voltages_array[1], g_bms_data.cell_voltages_array[2],
-                  g_bms_data.cell_voltages_array[3], g_bms_data.cell_voltages_array[4], g_bms_data.cell_voltages_array[5],
-                  g_bms_data.cell_voltages_array[6], g_bms_data.cell_voltages_array[7], g_bms_data.cell_voltages_array[8]);
-            LOG_I("Total Voltage: %d mV", g_bms_data.battery_voltage);
+            LOG_E("Failed to read cell voltages\r\n");
         }
-        else
+        if (BQ76940_ReadCurrent(&g_bms_data.battery_current) != 1)
         {
-            LOG_E("Failed to read cell voltages");
-        }
-        if (BQ76940_ReadCurrent(&g_bms_data.battery_current) == 1)
-        {
-            LOG_I("Current: %d mA", g_bms_data.battery_current);
-        }
-        else
-        {
-            LOG_E("Failed to read current");
+            LOG_E("Failed to read current\r\n");
         }
 
-        if (BQ76940_ReadTemperature(&g_bms_data.temperature) == 1)
+        if (BQ76940_ReadTemperature(&g_bms_data.temperature) != 1)
         {
-            LOG_I("Temperature: %d.%d", g_bms_data.temperature / 10, g_bms_data.temperature % 10);
+            LOG_E("Failed to read temperature\r\n");
         }
-        else
-        {
-            LOG_E("Failed to read temperature");
-        }
-
+        SOC_Calculation();
+        Cell_Voltage_Analysis();
+        PrintData();
     }
+}
+
+static void SOC_Calculation(void)
+{
+    // 简单的SOC计算示例，实际应用中应使用更复杂的算法
+    uint16_t total_voltage = g_bms_data.battery_voltage;
+    if (total_voltage >= BMS_BATTERY_VOLTAGE_MAX)
+        g_bms_data.battery_soc = 100;
+    else if (total_voltage >= BMS_BATTERY_VOLTAGE_MIN)
+        g_bms_data.battery_soc = (total_voltage - BMS_BATTERY_VOLTAGE_MIN) * 100 / (BMS_BATTERY_VOLTAGE_MAX - BMS_BATTERY_VOLTAGE_MIN);
+    else
+        g_bms_data.battery_soc = 0;
+}
+
+static void Cell_Voltage_Analysis(void)
+{
+    g_bms_data.cell_max_voltage = 0;
+    g_bms_data.cell_min_voltage = 0xFFFF;
+    uint16_t voltage = 0;
+
+    for (int i = 0; i < BMS_NUM_CELLS; i++)
+    {
+        voltage = g_bms_data.cell_voltages_array[i];
+
+        if (voltage > g_bms_data.cell_max_voltage)
+        {
+            g_bms_data.cell_max_voltage = voltage;
+        }
+        if (voltage < g_bms_data.cell_min_voltage)
+        {
+            g_bms_data.cell_min_voltage = voltage;
+        }
+    }
+
+    g_bms_data.cell_max_voltage_difference = g_bms_data.cell_max_voltage - g_bms_data.cell_min_voltage;
+    g_bms_data.cell_avg_voltage = g_bms_data.battery_voltage / BMS_NUM_CELLS;
+}
+
+static void PrintData(void)
+{
+    float current_a = g_bms_data.battery_current / 1000.0f; // 转换为A
+    float temperature_c = g_bms_data.temperature / 10.0f;   // 转换为℃
+
+    LOG_I("/******************* BMS Data *******************/\r\n");
+    LOG_I("Battery SOC: %d%%\r\n\r\n", g_bms_data.battery_soc);
+
+    LOG_I("Cell Max Voltage: %d.%03dV\r\n", g_bms_data.cell_max_voltage / 1000, g_bms_data.cell_max_voltage % 1000);
+    LOG_I("Cell Min Voltage: %d.%03dV\r\n", g_bms_data.cell_min_voltage / 1000, g_bms_data.cell_min_voltage % 1000);
+    LOG_I("Cell Max Voltage Difference: %01d.%03dV\r\n", g_bms_data.cell_max_voltage_difference / 1000, g_bms_data.cell_max_voltage_difference % 1000);
+    LOG_I("Cell Avg Voltage: %d.%03dV\r\n\r\n", g_bms_data.cell_avg_voltage / 1000, g_bms_data.cell_avg_voltage % 1000);
+
+    LOG_I("Battery Voltage: %d.%03dV\r\n", g_bms_data.battery_voltage / 1000, g_bms_data.battery_voltage % 1000);
+    LOG_I("Battery Current: %.3fA\r\n", current_a);
+    LOG_I("Battery Temperature: %.1f\r\n\r\n", temperature_c);
+
+    LOG_I("Cell1 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[0] / 1000, g_bms_data.cell_voltages_array[0] % 1000);
+    LOG_I("Cell2 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[1] / 1000, g_bms_data.cell_voltages_array[1] % 1000);
+    LOG_I("Cell3 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[2] / 1000, g_bms_data.cell_voltages_array[2] % 1000);
+    LOG_I("Cell4 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[3] / 1000, g_bms_data.cell_voltages_array[3] % 1000);
+    LOG_I("Cell5 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[4] / 1000, g_bms_data.cell_voltages_array[4] % 1000);
+    LOG_I("Cell6 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[5] / 1000, g_bms_data.cell_voltages_array[5] % 1000);
+    LOG_I("Cell7 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[6] / 1000, g_bms_data.cell_voltages_array[6] % 1000);
+    LOG_I("Cell8 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[7] / 1000, g_bms_data.cell_voltages_array[7] % 1000);
+    LOG_I("Cell9 Voltage: %d.%03dV\r\n", g_bms_data.cell_voltages_array[8] / 1000, g_bms_data.cell_voltages_array[8] % 1000);
+    LOG_I("/***********************************************/\r\n\r\n");
 }
--- User/APP/bms_global.h
+++ User/APP/bms_global.h
@@ -3,6 +3,31 @@
 
 #include "main.h"
 
+#define BMS_NUM_CELLS 9
+
+#define BMS_SCD_THRESHOLD 2000 // 放电短路电流阈值，单位：mA
+#define BMS_SCD_DELAY 10       // 短路保护触发延时，单位：ms
+#define BMS_OCD_THRESHOLD 1000 // 过流电流阈值，单位：mA
+#define BMS_OCD_DELAY 1000     // 过流保护触发延时，单位：ms
+
+#define BMS_OV_THRESHOLD 4250 // 过压阈值，单位：mV
+#define BMS_OV_DELAY 1000     // 过压保护触发延时，单位：ms
+#define BMS_UV_THRESHOLD 3200 // 欠压阈值，单位：mV
+#define BMS_UV_DELAY 1000     // 欠压保护触发延时，单位：ms
+
+#define BMS_OT_THRESHOLD 80  // 过温阈值，单位：℃
+#define BMS_OT_DELAY 1000    // 过温保护触发延时，单位：ms
+#define BMS_UT_THRESHOLD -20 // 欠温阈值，单位：℃
+#define BMS_UT_DELAY 1000    // 欠温保护触发延时，单位：ms
+
+#define BMS_BALANCE_VOLTAGE_THRESHOLD 4100          // 平衡电压阈值，单位：mV
+#define BMS_BALANCE_VOLTAGE_DIFFERENCE_THRESHOLD 50 // 平衡电压差阈值，单位：mV
+
+#define BMS_CELL_VOLTAGE_MAX 4200 // 单体电池最高电压，单位：mV
+#define BMS_CELL_VOLTAGE_MIN 3000 // 单体电池最低电压，单位：mV
+
+#define BMS_BATTERY_VOLTAGE_MAX (BMS_CELL_VOLTAGE_MAX * BMS_NUM_CELLS) // 电池组最高电压，单位：mV
+#define BMS_BATTERY_VOLTAGE_MIN (BMS_CELL_VOLTAGE_MIN * BMS_NUM_CELLS) // 电池组最低电压，单位：mV
 typedef struct
 {
     uint16_t battery_soc; // 电池组剩余电量百分比，单位：%
@@ -16,7 +41,7 @@ typedef struct
     int16_t battery_current;  // 电池组电流，单位：mA
     int16_t temperature;      // 电池组温度，单位：℃
 
-    uint16_t cell_voltages_array[9]; // 9个单体电压数组，单位：mV
+    uint16_t cell_voltages_array[BMS_NUM_CELLS]; // 单体电池电压数组，单位：mV
 } BMS_Data_t;
 
 extern BMS_Data_t g_bms_data;
